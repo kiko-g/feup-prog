58ac57a5d5cb725a2720b383f4ea01f985e816be
Francisco Gonçalves <kikojpgoncalves@gmail.com>
2019-04-27 19:28:27 +0100

    leaving for a while

--- tryouts/prog1.cpp
+++ tryouts/prog1.cpp
@@ -2,13 +2,30 @@
 #include <iomanip>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <cstdlib>
 #include "p.h"
 using namespace std;
+
+class Rect
+{
+   private:
+    double width;
+    double height;
+
+   public:
+    Rect(double w, double h) { width = w; height = h; }
+    double circum() { return width * height; }
+};
 
 int main()
 {
     start(3);
+    Rect foo (10, 5);      // functional form
+    Rect baz {3, 15};      // uniform init.
+    Rect qux = {4, 12};    // POD-like
+
+    cout << "FOO: " << foo.circum() << "\n";
+    cout << "BAZ: " << baz.circum() << "\n";
+    cout << "QUX: " << qux.circum();
     end(3);
+    return 0;
 }
