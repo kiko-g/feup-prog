db8f828bc076e5addafe50b4f02b530b159c54fc
Francisco Gonçalves <kikojpgoncalves@gmail.com>
2019-05-14 12:08:22 +0100

    proj2

--- proj1/myproj/main.cpp
+++ proj1/myproj/main.cpp
@@ -1,6 +1,19 @@
 /*
 Text files are in a folder called 'textfiles' inside the 'myproj' folder
 */
+#include "Agency.h"
+#include "Menus.h"
+// #include "utils.h"
+
+int main()
+{
+    Agency agency(AGENCY_FILE_NAME); // create the agency
+
+    mainMenu(agency); // initial menu inicial with the major options of the application
+
+    return 0;
+}
+
 #include "myLib.h"
 using namespace std;
 
